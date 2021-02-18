11ef970d7c83c3c66249f7c5f1174d40d0b5f5a3
d-vo <dominik@van-opdenbosch.de>
2021-02-18 21:49:35 +0100

    Initial commit

---
+++ backup/scripts/params.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = "OBI"
+__copyright__ = "Copyright 2021, DPV e.V."
+__license__ = "MIT"
+
+PARAMS = {
+    'DATA_DIR': '',
+    'BACKUP_DIR': '',
+    'POSTGRES_HOST': '',
+    'POSTGRES_USER': '',
+    'POSTGRES_PASSWORD': '',
+    'POSTGRES_DB': '',
+    'SEND_MAIL': '',
+    'SMTP_TO': '',
+    'SMTP_FROM': '',
+    'SMTP_PWD': '',
+    'SMTP_HOST': '',
+    'SMTP_PORT': '',
+}
